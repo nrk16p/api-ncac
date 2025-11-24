98864c9568cb63429cbb1b91cb17c9adc02a0c20
nrk72p <hardman16p@gmail.com>
2025-11-24 11:20:55 +0700

    fix: add missing position schema

--- schemas/position_schema.py
+++ schemas/position_schema.py
@@ -1,10 +1,14 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
+# ============================================================
+# POSITION LEVEL
+# ============================================================
 class PositionLevelBase(BaseModel):
     level_name: str
 
+class PositionLevelCreate(PositionLevelBase):
+    pass
 
 class PositionLevelResponse(PositionLevelBase):
     position_level_id: int
@@ -13,15 +17,16 @@ class PositionLevelResponse(PositionLevelBase):
         from_attributes = True
 
 
+# ============================================================
+# POSITION
+# ============================================================
 class PositionBase(BaseModel):
     position_name_th: str
     position_name_en: str
-    position_level_id: int
-
+    position_level_id: Optional[int] = None
 
 class PositionCreate(PositionBase):
     pass
-
 
 class PositionResponse(PositionBase):
     position_id: int
