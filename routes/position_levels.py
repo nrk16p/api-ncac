0a893804a0b617486aa7b4bea747f4472b5e188a
nrk72p <hardman16p@gmail.com>
2025-11-24 11:05:39 +0700

    fix: add missing position schema

--- routes/position_levels.py
+++ routes/position_levels.py
@@ -6,7 +6,7 @@ import models, schemas
 router = APIRouter(prefix="/position-levels", tags=["Position Levels"])
 
 
-@router.post("/", response_model=schemas.PositionLevel)
+@router.post("/", response_model=schemas.PositionLevelResponse)
 def create_position_level(payload: schemas.PositionLevelCreate, db: Session = Depends(get_db)):
     new_level = models.PositionLevel(**payload.dict())
     db.add(new_level)
@@ -15,6 +15,6 @@ def create_position_level(payload: schemas.PositionLevelCreate, db: Session = De
     return new_level
 
 
-@router.get("/", response_model=list[schemas.PositionLevel])
+@router.get("/", response_model=list[schemas.PositionLevelResponse])
 def get_position_levels(db: Session = Depends(get_db)):
     return db.query(models.PositionLevel).all()
