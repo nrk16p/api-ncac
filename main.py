86787c86b2919e8e7eb9256ea69c1081fa697f53
Menatransport_02 <menatransport_02@Menatransport-02s-MacBook-Air.local>
2026-02-23 15:21:49 +0700

    allocation

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ app.include_router(form_master_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
